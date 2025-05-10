640d60a5786f7192ebf4ef7f94fb9ba64830585b
Stepanekmi <rawer@seznam.cz>
2025-05-10 15:08:53 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
 
+import os
 import discord
 from discord.ext import commands
 from discord import app_commands, Attachment
 from PIL import Image
 import io
-import os
 
 from ocr_utils import ocr_vs  # importuj OCR funkci z vlastního souboru
 
@@ -15,10 +15,8 @@ class VSBot(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         super().__init__(command_prefix="!", intents=intents)
-        self.tree = app_commands.CommandTree(self)
 
     async def setup_hook(self):
-        # Registrace příkazů jen pro konkrétní server (guild)
         guild = discord.Object(id=int(GUILD_ID))
         self.tree.copy_global_to(guild=guild)
         await self.tree.sync(guild=guild)
@@ -30,14 +28,11 @@ bot = VSBot()
 async def vs(interaction: discord.Interaction, obrazek: Attachment):
     await interaction.response.defer()
     try:
-        # Načti obrázek z přílohy
         img_bytes = await obrazek.read()
         img = Image.open(io.BytesIO(img_bytes))
 
-        # Použij OCR
         vysledky = ocr_vs(img)
 
-        # Odešli výsledek
         await interaction.followup.send(f"Výsledek OCR:\n```{vysledky}```")
     except Exception as e:
         await interaction.followup.send(f"Nastala chyba při zpracování obrázku: {e}")
